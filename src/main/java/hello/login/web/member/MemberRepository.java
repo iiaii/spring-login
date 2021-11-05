bf4d09a510e2d13b5088262be46239c6ca4e9097
iiaii <jhun9409@naver.com>
2021-11-05 23:51:31 +0900

    feat: 회원가입 폼 및 멤버 컨트롤러 구성

---
+++ src/main/java/hello/login/web/member/MemberRepository.java
@@ -0,0 +1,36 @@
+package hello.login.web.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Slf4j
+@Repository
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Member save(final Member member) {
+        member.setId(++sequence);
+        log.info("save: member={}", member);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(final Long id) {
+        return store.get(id);
+    }
+
+    public Optional<Member> findByLoginId(final String loginId) {
+        return findAll().stream()
+                .filter(member -> member.getLoginId().equals(loginId))
+                .findFirst();
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+}
