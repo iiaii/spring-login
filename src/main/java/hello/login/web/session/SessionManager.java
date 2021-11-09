b9f0f0d009a18f9e86b5248ac30b968dc88e0311
iiaii <jhun9409@naver.com>
2021-11-09 23:57:31 +0900

    feat: 세션 매니저 구현중

---
+++ src/main/java/hello/login/web/session/SessionManager.java
@@ -0,0 +1,59 @@
+package hello.login.web.session;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 세션 관리
+ */
+@Component
+public class SessionManager {
+
+    private static final String SESSION_COOKIE_NAME = "mySessionId";
+
+    private final Map<String, Object> sessionStore = new ConcurrentHashMap<>();
+
+    /**
+     * 세션 생성
+     * <p>
+     * sessionId 생성 (임의의 추정 불가능한 랜덤 값)
+     * 세션 저장소에 sessionId와 보관할 값 저장
+     * sessionId로 응답 쿠키를 생성해서 클라이언트에 전달
+     *
+     * @param value
+     * @param response
+     */
+    public void createSession(final Object value, final HttpServletResponse response) {
+        // 세션 id를 생성하고, 값을 세션에 저장
+        String sessionId = UUID.randomUUID().toString();
+        sessionStore.put(sessionId, value);
+
+        // 쿠키 생성
+        Cookie cookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
+        response.addCookie(cookie);
+    }
+
+
+    public Object getSession(final HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        return Optional.ofNullable(sessionCookie)
+                .map(sessionStore::get)
+                .orElse(null);
+    }
+
+    public Cookie findCookie(final HttpServletRequest request, final String cookieName) {
+        Cookie[] cookies = request.getCookies();
+        if (Objects.isNull(cookies)) {
+            return null;
+        }
+        return Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals(SESSION_COOKIE_NAME))
+                .findAny()
+                .orElse(null);
+    }
+}
